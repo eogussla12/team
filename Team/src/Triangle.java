6d1cf7daf189e4883a1b280bcfda23bf7bdf9a69
jooo529 <jooo529@gmail.com>
2016-04-27 16:01:29 +0900

    programA

---
+++ Team/src/Triangle.java
@@ -0,0 +1,63 @@
+
+import java.lang.*;
+
+public class Triangle {
+	
+	double side1;
+	double side2;
+	double side3;
+	
+	public Triangle(double side1, double side2, double side3) {
+		this.side1 = side1;
+		this.side2 = side2;
+		this.side3 = side3;
+	}
+
+	public double getSide1() {
+		return side1;
+	}
+
+	public void setSide1(double side1) {
+		if(side1>0)
+		this.side1 = side1;
+	}
+
+	public double getSide2() {
+		return side2;
+	}
+
+	public void setSide2(double side2) {
+		if(side2>0)
+		this.side2 = side2;
+	}
+
+	public double getSide3() {
+		return side3;
+	}
+
+	public void setSide3(double side3) {
+		if(side3>0)
+		this.side3 = side3;
+	}
+	
+	double calculateArea(){
+		double sideSum = (getSide1()+getSide2()+getSide3())/2;
+		double sumM1 = sideSum-getSide1();
+		double sumM2 = sideSum-getSide2();
+		double sumM3 = sideSum-getSide3();
+		
+		double areaTriangle = Math.sqrt(sideSum*sumM1*sumM2*sumM3);
+		
+		return areaTriangle;
+	}
+	
+	double calculatePerimeter(){ 
+		return getSide1()+getSide2()+getSide3();
+	}
+	
+	void printinfo(){
+		
+	}
+	
+	
+}
