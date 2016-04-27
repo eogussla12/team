a2a22fbadf4db06d4ae42f093a4bf5507a723221
Park-Seonghyeon <dbtn751@naver.com>
2016-04-27 15:52:12 +0900

    PPPSH create Ractangle, ShapeTest

---
+++ Team/src/Rectangle.java
@@ -0,0 +1,24 @@
+
+
+public class Rectangle {
+	private double width;
+	private double height;
+	
+	public Rectangle(double width, double height){
+		this.width=width;
+		this.height=height;
+	}
+	
+	public double calculateArea(){
+		return 0;
+	}
+	
+	public double calculatePerimeter(){
+		return 0;
+	}
+	
+	public void printInfo(){
+		
+	}
+	
+}
---
+++ Team/src/ShapeTest.java
@@ -0,0 +1,16 @@
+
+
+public class ShapeTest {
+		
+	 public void ShapeTest(){
+		 
+	 }
+	 
+	 public static void main(String[] args){
+		 
+	 }
+	 
+	 public void processShape(){
+		 
+	 }
+}
