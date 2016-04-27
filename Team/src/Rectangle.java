2aaef3de1132b04b9a834b1b01f448d7ee28cd79
jooo529 <ajou@ajou-PC>
2016-04-27 16:18:52 +0900

    prA

--- Team/src/Rectangle.java
+++ Team/src/Rectangle.java
@@ -22,7 +22,18 @@ public class Rectangle {
 	}
 	
 	public void drawRectangle(){
-		
+		for(int i=0; i<(int) height;i++){
+			if(i==0||i==(int)height-1)	System.out.print("+");
+			else System.out.print("|");
+			for(int j=0; j<(int)width-2; j++){
+				System.out.print("-");
+			}
+			if(i==0||i==(int)height-1)
+				System.out.print("+");
+			else System.out.print("|");
+			System.out.println();
+		}
 	}
+	
 	
 }
